0e88aa0b0cb47c79e6c1d93ce7dfccac9234e1ad
nizw0 <me@nizw0.dev>
2025-04-01 01:52:23 +0800

    chore: hw2 init

---
+++ hw2/calc.js
@@ -0,0 +1,19 @@
+class Calc {
+  static add(a, b) {
+    return a + b
+  }
+
+  static subtract(a, b) {
+    return null
+  }
+
+  static multiply(a, b) {
+    return null
+  }
+
+  static divide(a, b) {
+    return null
+  }
+}
+
+export default Calc
