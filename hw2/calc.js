86f2e382d809759b491c78a97fc3ddf54648febe
nizw0 <me@nizw0.dev>
2025-04-01 11:40:47 +0800

    feat(hw2): add type checking to Calc methods

--- hw2/calc.js
+++ hw2/calc.js
@@ -1,17 +1,31 @@
+import { NonNumericInputError } from './error'
+
 class Calc {
   static add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
     return a + b
   }
 
   static subtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
     return a - b
   }
 
   static multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
     return a * b
   }
 
   static divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
     if (b === 0) {
       return NaN
     }
