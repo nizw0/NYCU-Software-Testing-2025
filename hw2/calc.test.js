2455aade48198a996093e39ee914475aca913272
nizw0 <me@nizw0.dev>
2025-04-06 02:58:26 +0800

    chore: hw2 init

---
+++ hw2/calc.test.js
@@ -0,0 +1,13 @@
+import { describe, test } from 'vitest'
+
+describe('subtract operation', () => {
+  test.todo('not implemented yet')
+})
+
+describe('multiply operation', () => {
+  test.todo('not implemented yet')
+})
+
+describe('divide operation', () => {
+  test.todo('not implemented yet')
+})
