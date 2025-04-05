4dc6fd40fa93eeb6974b2cb8bcbe595c19240ebb
nizw0 <me@nizw0.dev>
2025-04-06 03:44:27 +0800

    feat(hw2): implement Calc methods

--- hw2/calc.js
+++ hw2/calc.js
@@ -1,18 +1,38 @@
+import { NonNumericInputError } from './errors'
 class Calc {
   static add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
+
     return a + b
   }
 
   static subtract(a, b) {
-    return null
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
+
+    return a - b
   }
 
   static multiply(a, b) {
-    return null
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
+
+    return a * b
   }
 
   static divide(a, b) {
-    return null
+    if (typeof a !== 'number' || typeof b !== 'number') {
+      throw new NonNumericInputError()
+    }
+    if (b === 0) {
+      return NaN
+    }
+
+    return Math.round(a / b)
   }
 }
 
